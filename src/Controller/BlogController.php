511d7ce7c0188451c2409c06298fb4621289aaf5
ipalakot <ipalakot@gmail.com>
2019-11-13 14:01:35 +0100

    Adding article Form

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,6 +8,12 @@ use App\Entity\Article;
 use Zend\Code\Generator\DocBlock\Tag\ReturnTag;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class BlogController extends AbstractController
 {
@@ -33,10 +39,35 @@ class BlogController extends AbstractController
     /** 
      * @Route("/blog/nouveau", name="nouv.article")
     */
-    public function nouvelArticle(Request $request)
+    public function nouvelArticle(Request $request, ObjectManager $manager)
     {
-        Return $this->render('blog/nouveau.html.twig');
+        $article = new Article();
+        $form = $this->createFormBuilder($article) // Creer 1 formulaire lié à mon article;
+                    ->add('title', TextType::class, [
+                        'attr' => ['maxlength' => 2,
+                        'class'=>'form-control'
+                        ]
+                        ])
+
+                    ->add('content', TextareaType::class, [
+                        'attr' => ['placeholder' => "",
+                        'class'=>'form-control']
+                        ])
+                    
+                    ->add('image', TextType::class, [
+                            'attr' => ['placeholder' => "",
+                            'class'=>'form-control']])
+                    
+                        
+                    
+                    ->getForm();
+
+        return $this->render('blog/nouveau.html.twig', [
+               'formCreatArt' => $form->createView()
+               ]);
     }
+
+
     
     /**
      * @Route("/blog/{id}", name="blog_show")
