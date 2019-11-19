6b4828cc1e7148d5f3cdf46f0eee1b9455b43f9b
ipalakot <ipalakot@gmail.com>
2019-11-19 20:58:23 +0100

    Blocage de Admin

--- src/Controller/administrator/AdminController.php
+++ src/Controller/administrator/AdminController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Administrator;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Entity\Article;
 
 class AdminController extends AbstractController
 {
@@ -14,6 +18,25 @@ class AdminController extends AbstractController
     {
         return $this->render('administrator/admin/index.html.twig', [
             'controller_name' => 'AdminController',
+        ]);
+    }
+
+     /**
+     * @Route("/admininsitrator/article/blog", name="admin.article.blog")
+     */
+    public function listArticles(PaginatorInterface $paginator, Request $request)
+    {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        
+        $articles = $paginator->paginate(
+            $repo->findAll(),
+            $request->query->getInt('page', 1), /*page number*/
+             9 /*limit per page*/     );
+
+        
+        return $this->render('administrator/article/index.html.twig', [
+            'controller_name' => 'ArticleController',
+            'articles'=>$articles
         ]);
     }
 }
